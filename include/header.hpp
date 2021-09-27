d6e7e7857c089969edd8f74c77817fc4dab6281a
Pushkarev Nikita <push-trust@yandex.ru>
2021-09-27 10:48:00 +0300

    0.1

--- include/header.hpp
+++ include/header.hpp
@@ -2,5 +2,89 @@
 
 #ifndef INCLUDE_HEADER_HPP_
 #define INCLUDE_HEADER_HPP_
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+#include <nlohmann/json.hpp>
 
+using nlohmann::json;
+using namespace std;
+
+bool try_file(string jsonPath){
+  bool nofile = false;
+  std::ifstream file{jsonPath};
+  if (!file) {
+    nofile = true;
+    throw std::runtime_error{
+        "! The file cannot be found by this path:" + jsonPath};
+  }
+  return nofile;
+}
+
+bool no_size(json data){
+  bool nosize = false;
+  if (data["items"].size() == 0){
+    nosize = true;
+    throw std::runtime_error{"! File is empty"};
+  }
+  return nosize;
+}
+
+bool no_struct(json data){
+  bool nostruct = false;
+  if (data["_meta"]["count"] != data["items"].size()){
+    nostruct = true;
+    throw std::runtime_error{"! The structure of the json file is broken"};
+  }
+  return nostruct;
+}
+
+json Parse(string jsonPath){
+  std::ifstream file{jsonPath};
+  bool nofile = try_file(jsonPath);
+  if (nofile)
+    return 0;
+  json data;
+  file >> data;
+  bool nosize = no_size(data);
+  if (nosize)
+    return 0;
+  bool nostruct = no_struct(data);
+  if (nostruct)
+    return 0;
+  return data;
+}
+
+void Print(std::ostream& stream, json data){
+  string dbt;
+  string avg;
+  string name;
+  string group;
+  stream<<" |"<<std::setw( 20 ) << std::right << "name"
+         <<" |"<<std::setw( 15 ) << std::right << "group"
+         <<" |"<<std::setw( 8 ) << std::right << "avg"
+         <<" |"<<std::setw( 15 ) << std::right << "dbt" << " |" << endl;
+  stream<<" -------------------------------------------------------------------"
+         <<endl;
+  int t = data["items"].size();
+  for (int i = 0; i < t; i++) {
+    if (data["items"][i]["debt"] == "null")
+      stream<<to_string(data["items"][i]["debt"]);
+    if (int(data["items"][i]["debt"].size()) > 1)
+      dbt = to_string(data["items"][i]["debt"].size()) + " items";
+    else if(data["items"][i].at("debt").is_null())
+      dbt = "null";
+    else
+      dbt = data["items"][i].at("debt").get<string>();
+    name = data["items"][i].at("name").get<string>();
+    group = data["items"][i].at("group").get<string>();
+    avg = data["items"][i].at("avg").get<string>();
+    stream<<" |"<<std::setw( 20 ) << std::right << name
+           <<" |"<<std::setw( 15 ) << std::right << group
+           <<" |"<<std::setw( 8 ) << std::right << avg
+           <<" |"<<std::setw( 15 ) << std::right << dbt << " |" << endl;
+    stream<<" --------------------------------"
+              "-----------------------------------"<<endl;
+  }
+}
 #endif // INCLUDE_HEADER_HPP_
--- tests/test.cpp
+++ tests/test.cpp
@@ -1,7 +1,99 @@
-// Copyright 2020 Your Name <your_email>
-
 #include <gtest/gtest.h>
+#include <header.hpp>
 
-TEST(Example, EmptyTest) {
+TEST(ParseTest, TrueData) {
     EXPECT_TRUE(true);
+    string JSON_path = "..\\Students\\students.json";
+    string Tjson = ""
+        "{\n"
+        "  \"items\": [\n"
+        "    {\n"
+        "      \"name\": \"Ivanov Petr\",\n"
+        "      \"group\": \"1\",\n"
+        "      \"avg\": \"4.25\",\n"
+        "      \"debt\": null\n"
+        "    },\n"
+        "    {\n"
+        "      \"name\": \"Sidorov Ivan\",\n"
+        "      \"group\": \"31\",\n"
+        "      \"avg\": \"4\",\n"
+        "      \"debt\": \"C++\"\n"
+        "    },\n"
+        "    {\n"
+        "      \"name\": \"Pertov Nikita\",\n"
+        "      \"group\": \"IU8-31\",\n"
+        "      \"avg\": \"3.33\",\n"
+        "      \"debt\": [\n"
+        "        \"C++\",\n"
+        "        \"Linux\",\n"
+        "        \"Network\"\n"
+        "      ]\n"
+        "    }\n"
+        "  ],\n"
+        "  \"_meta\": {\n"
+        "    \"count\": 3\n"
+        "  }\n"
+        "}";
+    nlohmann::json TestFile = json::parse(Tjson);
+    nlohmann::json File = Parse(JSON_path);
+    EXPECT_EQ(TestFile, File);
 }
+
+TEST(ParseTest, try_file)
+{
+  string JSON_path = "..\\Students\\student.json";
+  string er = "! The file cannot be found by this path:" + JSON_path;
+  try {
+    try_file(JSON_path);
+    FAIL() << "! The file cannot be found by this path:" + JSON_path;
+  }
+  catch(std::runtime_error const & err) {
+    EXPECT_EQ(err.what(),std::string(er));
+  }
+}
+TEST(ParseTest, no_size)
+{
+  string JSON_path = "..\\Students\\test1.json";
+  std::ifstream file{JSON_path};
+  json data;
+  file >> data;
+  try {
+    no_size(data);
+    FAIL() << "! File is empty";
+  }
+  catch(std::runtime_error const & err) {
+    EXPECT_EQ(err.what(),std::string("! File is empty"));
+  }
+}
+
+TEST(ParseTest, Test3) {
+  string JSON_path = "..\\Students\\test2.json";
+  std::ifstream file{JSON_path};
+  json data;
+  file >> data;
+  try {
+    no_struct(data);
+    FAIL() << "! The structure of the json file is broken";
+  } catch (std::runtime_error const& err) {
+    EXPECT_EQ(err.what(),
+              std::string("! The structure of the json file is broken"));
+  }
+}
+
+TEST(Print, Test1) {
+  string Table = " |                name |          group |     avg |            dbt |\n"
+      " -------------------------------------------------------------------\n"
+      " |         Ivanov Petr |              1 |    4.25 |           null |\n"
+      " -------------------------------------------------------------------\n"
+      " |        Sidorov Ivan |             31 |       4 |            C++ |\n"
+      " -------------------------------------------------------------------\n"
+      " |       Pertov Nikita |         IU8-31 |    3.33 |        3 items |\n"
+      " -------------------------------------------------------------------\n";
+
+  std::stringstream StreamT;
+  string jsonPath = "..\\Students\\students.json";
+  json data = Parse(jsonPath);
+  Print(StreamT, data);
+  EXPECT_EQ(Table, StreamT.str());
+}
+
